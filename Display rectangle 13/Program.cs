bf8eb339276ef96bd433ace5bb9d2b94785ec12b
Rintu <Rintu@VINDUJA>
2022-10-31 16:30:42 +0530

    Add project files.

---
+++ Display rectangle 13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display_rectangle_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter any number");
+            int a=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}{0}{0}",a);
+            Console.WriteLine("{0} {0}", a);
+            Console.WriteLine("{0} {0}", a);
+            Console.WriteLine("{0}{0}{0}", a);
+            Console.ReadLine();
+        }
+    }
+}
